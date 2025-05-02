f43879e4f0a7415002f7048cac1d8e805ecc4899
Vitek Dvorak <vitek.dvorak@navigo3.com>
2025-05-02 21:57:22 +0200

    Created root main file

---
+++ RezervacniSystem/src/Kino/Kino.java
@@ -0,0 +1,22 @@
+package Kino;
+
+public class Kino {
+
+	public static void main(String[] args) {
+		/* Ask user what part of the system he wants to use
+		   1. Administration
+		   2. Reservation system
+		   2. Buffet
+		   
+		   More info in  main file of each option...
+		*/
+		
+		/*
+		 * Think about all classes that we would want to be saved, they need some ID field so we can identify records
+		 * It is necessary for example when you store an order it has a reference to an employee and it would be overhead storing whole employee directly into order
+		 * so we store only it's ID instead
+		 * */
+	
+	}
+
+}
